a467b895a9412c52c13fd378cb71495f63d06b46
mateusgabrielsilva <mateus92.mgs@gmail.com>
2020-03-24 20:38:41 -0300

    Comecando o back-end em node.js

---
+++ src/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333);
+
+/**
+ * Rota / Recurso
+ */
+
+ /**
+  * Metodos HTTP
+  * 
+  * GET : Buscar uma informação no back-end
+  * POST : Criar uma informacao no back-end
+  * PUT : Alterar uma informação no back-end
+  * DELETE : Deletar uma informação no back-end
+  */
+
+  /**
+   * Tipos de parametros 
+   * 
+   * Query : Parametros nomeados enviados na rota apos o "?" (Filtros, paginação)
+   * Route Params : Parametros utilizados para identificar recursos
+   * Request Body : Corpo da requisição, utilizando para criar ou alterar recursos
+   */
+
+   /**
+    * Banco de Dados
+    * 
+    * SQL : MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+    * No SQL : MongoDB, CouchDB, etc
+    */
+
+    /**
+     * Driver : SELECT * FROM users
+     * Query Buider: table('users).select
+     */
+
+
+
