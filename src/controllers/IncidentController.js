82eff34cb2a8b67e898ae77d60010d3d2f37d9b8
mateusgabrielsilva <mateus92.mgs@gmail.com>
2020-03-25 11:22:29 -0300

    Back-end finalizado

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -2,7 +2,22 @@ const connection = require('../database/connection');
 
 module.exports = {
     async index(request, response) {
-        const incidents = await connection('incidents').select('*');
+        const { page = 1 } = request.query;
+
+        const [count] = await connection('incidents').count();
+
+        const incidents = await connection('incidents')
+        .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+        .limit(5)
+        .offset((page - 1) * 5)
+        .select(['incidents.*',
+         'ongs.name',
+         'ongs.email',
+         'ongs.whatsapp',
+         'ongs.city',
+         'ongs.uf']);
+
+        response.header('X-Total-Count', count['count(*)']);
 
         return response.json(incidents);
     },
@@ -25,12 +40,9 @@ module.exports = {
         const { id } = request.params;
         const ong_id = request.headers.authorization;
 
-        const incident = await connection('incidents')
-            .where('id', id)
-            .select('ong_id')
-            .first();
+        const incidents = await connection('incidents').where('id', id).select('ong_id').first();
 
-        if (incident.ong_id != ong_id) {
+        if (incidents.ong_id != ong_id) {
             return response.status(401).json({ error: 'Operator not permitted.' });
         }
         
